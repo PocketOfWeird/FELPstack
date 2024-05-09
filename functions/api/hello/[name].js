9821355eb4bea09d3a325d1dd06da159aeb388f8
PocketOfWeird <hartzlernathan@gmail.com>
2024-05-09 14:04:41 -0500

    fixing assignment bug

--- functions/api/hello/[name].js
+++ functions/api/hello/[name].js
@@ -2,6 +2,6 @@ import { sayHello } from "../../../fsharp/hello/Program.fs.js";
 
 export function onRequest(context) {
     let name = context.params.name;
-    result = sayHello(name);
+    let result = sayHello(name);
     return new Response(result);
 }
